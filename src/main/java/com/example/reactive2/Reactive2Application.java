3874133f1359cb898c1f815fd919d3045970a911
juanczapata <jcarczapata@gmail.com>
2022-08-25 20:13:28 -0500

    first commit

---
+++ src/main/java/com/example/reactive2/Reactive2Application.java
@@ -0,0 +1,13 @@
+package com.example.reactive2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reactive2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reactive2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactive2/Reactive2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactive2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reactive2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
